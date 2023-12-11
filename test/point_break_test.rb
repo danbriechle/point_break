6160177501b779eef765fb68920b546278009217
Dan Briechle <danbriechle@hotmail.com>
2023-12-11 11:03:57 -0700

    Test: add happy and sad path testing

--- test/point_break_test.rb
+++ test/point_break_test.rb
@@ -6,6 +6,22 @@ require_relative '../app/point_break'
 
 class PointBreakTest < Minitest::Test
   def test_it_exists
-    assert_instance_of(PointBreak, PointBreak.new)
+    string = 'Wow neato!'
+    assert_instance_of(PointBreak, PointBreak.new(string))
+  end
+
+  def test_it_can_insert_break_points
+    terms = %w[I string]
+    string = 'I am a string'
+
+    assert_equal(PointBreak.new(string).break_on(terms), '<br>I</br>am a <br>string</br>')
+  end
+
+  # sad path
+  def test_it_does_not_break_on_partial_words
+    terms = %w[I string str]
+    string = 'I am a string'
+
+    assert_equal(PointBreak.new(string).break_on(terms), '<br>I</br>am a <br>string</br>')
   end
 end
