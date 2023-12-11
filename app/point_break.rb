ad44137bbe19c6d681d51e4628e67fc79271bc2c
Dan Briechle <danbriechle@hotmail.com>
2023-12-11 11:04:22 -0700

    Feat: impliment word break feature

--- app/point_break.rb
+++ app/point_break.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class PointBreak
+  def initialize(string)
+    @string = string
+  end
+
+  def break_on(arry)
+    new_arry = []
+
+    @string.split(' ').each do |word|
+      arry.include?(word) ? new_arry.push("<br>#{word}</br>") : new_arry.push("#{word} ")
+    end
+
+    new_arry.join
+  end
 end
